7b663e2d8955a42ede195a68716cf2b34f4d739d
DJ Hoffman <51058835+djhoffman@users.noreply.github.com>
2020-04-07 03:02:40 -0700

    Add files via upload

---
+++ Project2/Project2.ino
@@ -0,0 +1,47 @@
+
+//referenced: sensor example
+//implemented: RGB instead
+
+// inputs
+int red = 12;
+int green = 27;
+
+// PWM (pulse width modulation)
+const int freq = 5000;
+int ledChannel = 0;
+const int resolution = 8;
+
+void setup(){
+  // outputs
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  
+  // LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+
+  // attach the channel GPIO (general-purpose input/output)
+  ledcAttachPin(red, ledChannel);
+
+  // serial communication (bits per second)
+  Serial.begin(9600);
+
+}
+
+void loop(){
+  // analog pin A0
+  int sensorValue = analogRead(A0);
+  // print value
+  Serial.println(sensorValue);
+  // delay for stability
+  delay(1);
+
+  // INPUT
+  sensorValue = constrain(sensorValue, 200, 330);
+  int brightness = map(sensorValue, 200, 330, 0 , 255);
+  
+  //OUTPUT
+  ledcWrite(ledChannel, brightness);
+
+  // orange when sensor is high and green when low
+  digitalWrite(green, HIGH);
+}
